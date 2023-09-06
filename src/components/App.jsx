0293fc26c81612cf944427570bd2ab876fb740f9
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-09-07 00:02:36 +0300

    refactor App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,73 @@
 import { Section } from './Section/Section';
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { PhoneBook } from './PhoneBook/PhoneBook';
 import { ContactsList } from './Contacts/ContactsList';
 import { Filter } from './Filter/Filter';
 import { GlobalStyles } from './GlobalStyles.styled';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  addContact = newContact => {
-    if (this.state.contacts.some(contact => contact.name === newContact.name)) {
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts) {
+      setContacts(savedContacts);
+      console.log('Contacts loaded from localStorage:', savedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    console.log('Contacts saved to localStorage:', contacts);
+  }, [contacts]);
+
+  const addContact = newContact => {
+    if (contacts.some(contact => contact.name === newContact.name)) {
       alert('Contact with the same name already exists.');
     } else {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, { id: nanoid(5), ...newContact }],
-      }));
+      setContacts(prevContacts => {
+        return [...prevContacts, { id: nanoid(5), ...newContact }];
+      });
     }
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  handleChange = evt => {
-    const { value } = evt.target;
-    this.setState({ filter: value });
-  };
-
-  getFilterContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase().trim();
-    return this.state.contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(normalizedFilter);
+  const handleDeleteContact = id => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== id);
     });
   };
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
-  }
+  const handleChange = evt => {
+    setFilter(evt.target.value);
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const filteredContacts = this.getFilterContacts();
-
-    return (
-      <>
-        <Section title="Phone book">
-          <PhoneBook addNewContact={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter handleChange={this.handleChange} />
-          <ContactsList
-            contacts={filteredContacts}
-            onDeleteContact={this.handleDeleteContact}
-          />
-          {filteredContacts.length === 0 && <p>No contacts are availablee</p>}
-        </Section>
-        <GlobalStyles />
-      </>
+  const getFilterContacts = () => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
     );
-  }
-}
+
+    return filteredContacts;
+  };
+
+  const filteredContacts = getFilterContacts();
+  return (
+    <>
+      <Section title="Phone book">
+        <PhoneBook addNewContact={addContact} />
+      </Section>
+      <Section title="Contacts">
+        <Filter handleChange={handleChange} />
+        <ContactsList
+          contacts={filteredContacts}
+          onDeleteContact={handleDeleteContact}
+        />
+        {filteredContacts.length === 0 && <p>No contacts are available</p>}
+      </Section>
+      <GlobalStyles />
+    </>
+  );
+};
